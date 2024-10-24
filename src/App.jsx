dd533597ebdb744023da63cc095bbb00429992bb
HKrobinvai <hkrobin48@gmail.com>
2024-10-24 20:11:49 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+
+
+import './App.css'
+import Blogs from './Blogs/Blogs'
+import Header from './Components/Heder/Header'
+
+function App() {
+  
+
+  return (
+    <>
+      
+      <Header></Header>
+      <Blogs></Blogs>
+      
+    
+    </>
+  )
+}
+
+export default App
---
+++ src/Blogs/Blogs.jsx
@@ -0,0 +1,22 @@
+import { data } from "autoprefixer";
+import { useEffect } from "react";
+import { useState } from "react";
+
+
+const Blogs = () => {
+    const[blogs,setBlogs] = useState()
+
+    useEffect( ()=>{
+        fetch('bloge.json')
+        .then(res => res.json())
+        .then(data => setBlogs(data))
+    }, [])
+
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Blogs;
